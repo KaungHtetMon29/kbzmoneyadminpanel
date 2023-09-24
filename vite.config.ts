9d55e5e2331ad127f8affa815119da0483f78690
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-09-24 22:00:59 +0630

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        // Access environment variables
+        dir: `dist/${import.meta.env.VITE_APP_ENV}`,
+      },
+    },
+  },
+});
