a180cc4476dd6e81aada889aae9f35859abcc363
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-09-24 22:04:05 +0630

    test'

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        // Access environment variables
-        dir: `dist/${import.meta.env.VITE_APP_ENV}`,
-      },
-    },
-  },
 });
